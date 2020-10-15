cd9059b1250aef17f67ddacfdcc499920a08d013
Sam-Holton <samuel.holton@gmail.com>
2020-10-15 07:24:11 -0400

    First Commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Please enter a number between 1 and 100: ");
+                int userEntry = int.Parse(Console.ReadLine());
+
+                if (userEntry % 2 == 1)
+                {
+                    Console.WriteLine($"{userEntry} Odd");
+                }
+
+                if (userEntry % 2 == 0 && userEntry < 25)
+                {
+                    Console.WriteLine("Even and less than 25");
+                }
+
+                if (userEntry > 25 && userEntry < 61 && userEntry % 2 == 0)
+                {
+                    Console.WriteLine("Even");
+                }
+
+                if (userEntry >= 60 && userEntry % 2 == 0)
+                {
+                    Console.WriteLine($"{userEntry} Even");
+                }
+
+                if (userEntry >= 60 && userEntry % 2 == 1)
+                {
+                    Console.WriteLine($"{userEntry} Odd");
+                }
+
+                Console.Write("Continue? (y/n) ");
+                char userContinue = Console.ReadLine();
+
+            } while (userContinue == 'y');
+
+        }
+    }
+}
